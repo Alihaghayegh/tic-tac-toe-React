eabeaf4b4c7ce281cc9e00d9cda4d5dd5d165779
alihaghayegh <haghayeghali95@gmail.com>
2023-10-04 19:01:14 +0330

    spacing in footer resolved

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        Project Refactored by
+        Project Refactored by 
         <a href="https://www.twitter.com/aliha995">Ali haghayegh</a>
       </p>
       <p>
-        and
+        and 
         <a href="https://www.github.com/Alihaghayegh">Github Page</a>
       </p>
     </footer>
