74a76db0f3283956e9bc1bcf30db6811eb0f4e5d
alihaghayegh <haghayeghali95@gmail.com>
2023-10-05 21:23:37 +0330

    derivedGame function added to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,43 @@ const players: Player[] = [
   },
 ];
 
-function deriveGame(state: GameState) {}
+function deriveGame(state: GameState) {
+  const currentPlayer = players[state.currentGameMoves.length % 2];
+
+  const winnigPattens = [
+    [1, 2, 3],
+    [1, 5, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 5, 7],
+    [3, 6, 9],
+    [4, 5, 6],
+    [7, 8, 9],
+  ];
+
+  let winner = null;
+
+  for (const player of players) {
+    const selectedSquareIds = state.currentGameMoves
+      .filter((move) => move.player.id === player.id)
+      .map((move) => move.squareId);
+
+    for (const pattern of winnigPattens) {
+      if (pattern.every((v) => selectedSquareIds.includes(v))) {
+        winner = player;
+      }
+    }
+  }
+
+  return {
+    moves: state.currentGameMoves,
+    currentPlayer,
+    status: {
+      isComplete: winner != null || state.currentGameMoves.length === 9,
+      winner,
+    },
+  };
+}
 
 function deriveStats(state: GameState) {
   return {
